3e8d5c1a4073afcaefe9b726054df0ef1c39d881
Mukhtadir Ahmed <nighathanjum3@gmail.com>
2024-10-03 10:56:41 +0530

    Demo Code

---
+++ demo.java
@@ -0,0 +1,17 @@
+public class Demo{
+public static void main(String[] args){
+    byte name = 20;
+    byte animal = 30;
+    byte pencils =25;
+    byte bags = 35;
+    byte StudentsInGroup = 10;
+
+    short CarsInShop = 20000;
+String result= "pass";
+int grade=90;
+double marks=600.0;
+System.out.println(result);
+System.out.println(grade);
+System.out.println(marks);
+}
+}
